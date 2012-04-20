f952cfbd4ce60c5984871fe755b17536a456cd51
marcus <marcus@marcus-PC>
2012-04-20 22:46:31 +0200

    sadas

--- YesNNoes/src/yes/n/noes/func/AddDialog.java
+++ YesNNoes/src/yes/n/noes/func/AddDialog.java
@@ -1,11 +1,24 @@
 package yes.n.noes.func;
 
+import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.widget.EditText;
 
-public class AddDialog extends DialogFragment {
+public class AddDialog extends DialogFragment implements OnClickListener {
 
+	EditText ed;
+	
+	public interface OnAddListener{
+		public void AddCat(String value);
+	}
+	
+	private static OnAddListener listener;
+
+	
 	public static AddDialog newInstance(int title) {
 		AddDialog frag = new AddDialog();
 		Bundle args = new Bundle();
@@ -16,10 +29,28 @@ public class AddDialog extends DialogFragment {
 
 	@Override
 	public Dialog onCreateDialog(Bundle savedInstanceState) {
-
+		AlertDialog.Builder di = new AlertDialog.Builder(getActivity());
+		ed=new EditText();
 		
+		di.setTitle("Add Category");
+		di.setView(view)
+		di.setPositiveButton("add", this);
 		
 		return null;
 
+	}
+
+	public void onClick(DialogInterface dialog, int button) {
+		dialog.dismiss();
+		
+		if(button == DialogInterface.BUTTON_POSITIVE) {
+			listener.AddCat();
+		}
+		
+		
+	}
+	
+	static void SetOnAddListener(OnAddListener listener){
+		AddDialog.listener=listener;
 	}
 }
