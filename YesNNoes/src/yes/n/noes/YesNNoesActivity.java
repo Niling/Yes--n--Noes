67a6a287642135d4bbae69edd5c2a2f9f454e4e1
marcus <marcus@marcus-PC>
2012-04-20 21:54:44 +0200

    lol

--- YesNNoes/src/yes/n/noes/YesNNoesActivity.java
+++ YesNNoes/src/yes/n/noes/YesNNoesActivity.java
@@ -1,14 +1,28 @@
 package yes.n.noes;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 
-public class YesNNoesActivity extends Activity {
+
+public class YesNNoesActivity extends ListActivity {
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        // Hej
+        
+        
+
+        String[] lol={"hej","då"};
+        // Now create a new list adapter bound to the cursor.
+        // SimpleListAdapter is designed for binding to a Cursor.
+        ListAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,lol);
+
+           
+
+        // Bind to our new adapter.
+        setListAdapter(adapter);
     }
 }
