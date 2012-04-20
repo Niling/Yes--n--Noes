291e26431f5dfecbfce150d50e7a82f89c980cb5
Niklas <nikl.ekman@gmail.com>
2012-04-20 21:20:31 +0200

    First commit

---
+++ YesNNoes/src/yes/n/noes/YesNNoesActivity.java
@@ -0,0 +1,14 @@
+package yes.n.noes;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class YesNNoesActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        // Hej
+    }
+}
